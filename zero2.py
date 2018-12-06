abc32726ca0963d16d9dfafc06c2cc806ddb8e74
Syed Zeeshan <43026021+zeeshan27102000@users.noreply.github.com>
2018-12-06 13:00:50 +0530

    Delete zero2.py

--- zero2.py
+++
@@ -1,5 +0,0 @@
-from watson_developer_cloud import TextToSpeechV1
-import json
-text_to_speech = TextToSpeechV1(iam_apikey='sT5EVClqWy1I0XcxojSBsH4JYum6ngNrYszCGmopd42s',url='https://gateway-syd.watsonplatform.net/text-to-speech/api')
-pronunciation = text_to_speech.get_pronunciation('cmr university is big campus','en-US_AllisonVoice','ibm').get_result()
-print(json.dumps(pronunciation, indent=2))
