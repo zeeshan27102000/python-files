5216a9eac8eae603d1a90820c1538d174c1c8508
Syed Zeeshan <43026021+zeeshan27102000@users.noreply.github.com>
2018-12-06 13:13:49 +0530

    Delete zero2.py

--- zero2.py
+++
@@ -1,5 +0,0 @@
-from watson_developer_cloud import TextToSpeechV1
-text_to_speech = TextToSpeechV1(iam_apikey='sT5EVClqWy1I0XcxojSBsH4JYum6ngNrYszCGmopd42s',url='https://gateway-syd.watsonplatform.net/text-to-speech/api')
-with open('hello_world.wav', 'wb') as audio_file:
-    audio_file.write(text_to_speech.synthesize('Hello world','audio/wav','en-US_AllisonVoice').get_result().content0
-
